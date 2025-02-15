7ac1e8b50a661f825081d2b2d4aa3c14ffe8ae9e
Umesh Nagare <umeshnagare27@gmail.com>
2025-02-15 20:18:48 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import LandingPage from "./MainCard";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <main className="flex items-center justify-center h-screen">
-      <LandingPage />
+      <div className="min-h-screen  flex flex-col items-start ml-1 md:ml-3 sm:ml-1 space-y-1 justify-center w-full">
+        <h1 className="text-3xl font-bold leading-tight  tracking-tight">
+          ShadCN + Vite + React + JavaScript
+        </h1>
+        <p className="text-lg text-muted-foreground">
+          A template, fast, and modern UI setup using ShadCN and Vite.
+        </p>
+        <a href="https://github.com/Algoture/vite-template" target="_blank">
+          <Button variant="default">Github</Button>
+        </a>
+      </div>
     </main>
   );
 }
